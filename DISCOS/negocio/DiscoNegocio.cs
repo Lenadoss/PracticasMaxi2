17c41d182f127a71f5537629c75e95866ef04280
Lenadoss <odanelfrias@gmail.com>
2025-07-18 19:16:54 -0400

    Clase acceso a datos creada

--- DISCOS/negocio/DiscoNegocio.cs
+++ DISCOS/negocio/DiscoNegocio.cs
@@ -19,32 +19,29 @@ namespace negocio
 
 			try
 			{
-				conexion.ConnectionString = "server=(localdb)\\MSSQLLocalDB; database=DISCOS_DB; integrated security = true;";
+				conexion.ConnectionString = "server = (localdb)\\MSSQLlocalDB ; database = DISCOS_DB; integrated security = true; ";
 				comando.CommandType = System.Data.CommandType.Text;
-				comando.CommandText = "SELECT d.id, d.titulo, d.fechalanzamiento, d.cantidadcanciones, d.urlimagentapa, e.Descripcion AS Estilo, te.Descripcion As Edicion FROM DISCOS d, ESTILOS e, TIPOSEDICION te WHERE d.idestilo = e.id AND d.idtipoedicion = te.id;";
+				comando.CommandText = "SELECT d.id, d.titulo, d.fechalanzamiento, d.cantidadcanciones, d.urlimagentapa, e.descripcion Estilo, te.descripcion Edicion FROM discos d, estilos e, tiposedicion te WHERE idestilo = e.id AND idtipoedicion = te.id";
 				comando.Connection = conexion;
-
-				conexion.Open();
+                conexion.Open();
 				lector = comando.ExecuteReader();
 
 				while(lector.Read())
 				{
 					Disco aux = new Disco();
 					aux.Id = (int)lector["id"];
-                    aux.Titulo = (string)lector["titulo"];
+					aux.Titulo = (string)lector["titulo"];
 					aux.FechaLanzamiento = (DateTime)lector["fechalanzamiento"];
-					aux.CantidadCanciones = (int)lector["cantidadcanciones"];
+					aux.CantidadCanciones = (int)lector["CantidadCanciones"];
 					aux.UrlImagenTapa = (string)lector["urlimagentapa"];
 					aux.Estilo = new Estilo();
 					aux.Estilo.Descripcion = (string)lector["Estilo"];
 					aux.Edicion = new Edicion();
 					aux.Edicion.Descripcion = (string)lector["Edicion"];
-
 					lista.Add(aux);
-                }
-
-                return lista;
-			}
+				}
+				return lista;
+            }
 			catch (Exception ex)
 			{
 				throw ex;
---
+++ DISCOS/negocio/EdicionNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class EdicionNegocio
+    {
+        public List<Edicion> listar ()
+        {
+			SqlConnection conexion = new SqlConnection();
+			SqlCommand comando = new SqlCommand();
+			SqlDataReader lector;
+			List<Edicion> lista = new List<Edicion>();
+			try
+			{
+				conexion.ConnectionString = "server=(localdb)\\MSSQLLocalDB; database=DISCOS_DB; integrated security = true;";
+				comando.CommandType = System.Data.CommandType.Text;
+				comando.CommandText = "Select id,descripcion FROM TIPOSEDICION";
+				comando.Connection = conexion;
+				conexion.Open();
+				lector = comando.ExecuteReader();
+
+				while (lector.Read())
+				{
+					Edicion aux = new Edicion();
+					aux.Id = (int)lector["id"];
+					aux.Descripcion = (string)lector["descripcion"];
+					lista.Add(aux);
+				}
+
+                return lista;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			finally
+			{
+				conexion.Close();
+			}
+        }
+    }
+}
